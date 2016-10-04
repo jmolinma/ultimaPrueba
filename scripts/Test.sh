397ec7f4e6629a3944695507a37c4c170c034c96
Javier Molina <j.sheerp@gmail.com>
2016-10-05 01:25:59 +0200

    Ini

---
+++ scripts/Test.sh
@@ -0,0 +1,15 @@
+#echo off
+export workspace=/Users/javierm/Documents/workspaceCV/javier.molina.SVC.miw.upm.es
+#set PATH=%PATH%C:\Program Files (x86)\Java\jre1.8.0_51\bin;C:\Users\bm0827\Desktop\apache-maven-3.3.3\bin
+#set JAVA_HOME=C:\Program Files (x86)\Java\jre1.8.0_51
+echo -----------------------------------------
+echo ".(C) MIW"
+echo -----------------------------------------
+echo .
+echo Workspace --- $workspace
+echo .
+cd $workspace
+echo "============ mvn clean test (profile: develop) ======================================================="
+echo .
+mvn clean test
+exit
